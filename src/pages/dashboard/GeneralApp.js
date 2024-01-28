015cbcdf0c3239605dcce5444369545b683f96b9
aimankhan <aiman.khan.personal.78605@gmail.com>
2024-01-28 19:43:22 +0530

    Created Conversation  Section

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Chat from "./Chat";
 import { Box, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Conversation from "../../components/conversation";
 
 const GeneralApp = () => {
+  const theme=useTheme();
   return (
     <Stack direction={"row"} sx={{ width: "100%" }}>
       <Chat />
-      {/*Conversation*/}
-      <Box sx={{height:"100%",width:"calc(100vw-420px)",backgroundColor:"#000"}}>
 
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(101vw - 420px)",
+          backgroundColor:theme.palette.mode === "light" ? "#fff":theme.palette.background.default,
+        }}
+      >
+        {/*Conversation*/}
+
+        <Conversation/>
       </Box>
     </Stack>
   );
